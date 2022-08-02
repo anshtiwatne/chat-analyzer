79c50877a0e07dad083d52b0e8f11caecc978a49
Ansh <ansh.tiwatne@gmail.com>
2022-08-02 19:36:54 +0530

    grouped stats in a __init__

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """Script to analyze WhatsApp chats"""
 
 import datetime
+import emoji
 import re
 
 
@@ -13,18 +14,21 @@ def get_data(path: str):
         pattern = r"(\d*?/\d*?/\d*?), (\d*?:\d*? [A, P]M) - (.*?): (.*)"
         data = re.findall(pattern, file.read())
 
-    df = {"dt": [], "sender": [], "msg": []}
+    df = {"timestamp": [], "sender": [], "msg": []}
 
     for element in data:
         date, time, sender, message = element
         dt = datetime.datetime.strptime(f"{date} {time}", "%m/%d/%y %I:%M %p")
 
-        df["dt"].append(dt)
+        df["timestamp"].append(dt)
         df["sender"].append(sender)
         df["msg"].append(message)
 
     return df
+
+
 df = get_data("testchat.txt")
+
 
 class User:
     """
@@ -32,24 +36,44 @@ class User:
     """
 
     def __init__(self, name: str):
-        self.name = name
 
-        messages = []
+        self.name = name
+        self.messages = []  # list of all of the user's messages in the chat
         for i, sender in enumerate(df["sender"]):
             if sender != self.name: continue
-            messages.append(df["msg"][i])
+            self.messages.append(df["msg"][i])
 
-        self.messages = messages
-        self.average_msg_length = sum(len(msg) for msg in self.messages) / len(self.messages)
-        self.longest_msg = max(self.messages, key=len)
-        self.words_typed = sum(len(msg.split()) for msg in self.messages)
-        self.top_words = sorted(set(word for msg in self.messages for word in msg.split()), key=lambda x: x.lower())
-        # we could add a few more of these, like time of day most texted, etc.
+        self.num_messages, self.num_words, self.num_emojis = 0, 0, 0
+        self.word_freq, self.emoji_freq = {}, {}
+        self.longest_msg = ""
+        max_len = 0
 
-    @property
-    def get_name(self):
-        """
-        Returns the user's name
-        """
+        for msg in self.messages:
+            self.num_messages += 1
+            if len(msg) > max_len:
+                max_len = len(msg)
+                self.longest_msg = msg
 
-        return self.name
+            for word in msg.split():
+                self.num_words += 1
+                if word not in self.word_freq: self.word_freq[word] = 1
+                else: self.word_freq[word] += 1
+
+                for char in word:
+                    if not emoji.is_emoji(char): continue
+                    self.num_emojis += 1
+                    if char not in self.emoji_freq: self.emoji_freq[char] = 1
+                    else: self.emoji_freq[char] += 1
+
+        self.word_emoji_ratio = self.num_emojis / self.num_words
+        self.avg_msg_len = self.num_words / self.num_messages  # words per message
+        self.word_freq = dict(
+            sorted(self.word_freq.items(), key=lambda x: x[1], reverse=True))
+        self.emoji_freq = dict(
+            sorted(self.emoji_freq.items(), key=lambda x: x[1], reverse=True))
+
+        hours = {hour:0 for hour in range(0, 24)}
+        for timestamp in df["timestamp"]:
+            print(timestamp)
+
+ansh = User("Ansh")
