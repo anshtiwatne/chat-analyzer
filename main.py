b99c2160f742c78a8ca64584b34f0c05990c520f
Ansh <ansh.tiwatne@gmail.com>
2022-08-03 17:01:19 +0530

    nested loops more efficient

--- main.py
+++ main.py
@@ -1,79 +1,78 @@
 """Script to analyze WhatsApp chats"""
 
-import datetime
-import emoji
+import dataclasses
+from datetime import datetime as dt
 import re
+import emoji
+import pandas as pd
 
 
-def get_data(path: str):
-    """
-    Reads in a file and returns a dataframe
-    """
+def frame_data(path: str):
+    """Reads a WhatsApp chat export file and creates a dataframe"""
 
-    with open(path, 'r', encoding="utf8") as file:
+    with open(path, "r", encoding="UTF8") as file:
+        # group1- date, group2- time, group3- sender, group4- message
         pattern = r"(\d*?/\d*?/\d*?), (\d*?:\d*? [A, P]M) - (.*?): (.*)"
         data = re.findall(pattern, file.read())
 
-    df = {"timestamp": [], "sender": [], "msg": []}
-
+    df = pd.DataFrame(columns=["timestamp", "user", "message"])
     for element in data:
         date, time, sender, message = element
-        dt = datetime.datetime.strptime(f"{date} {time}", "%m/%d/%y %I:%M %p")
-
-        df["timestamp"].append(dt)
-        df["sender"].append(sender)
-        df["msg"].append(message)
+        timestamp = dt.strptime(f"{date} {time}", "%m/%d/%y %I:%M %p")
+        df.loc[len(df)] = [timestamp, sender, message]
 
     return df
 
 
-df = get_data("testchat.txt")
-
-
 class User:
-    """
-    Class to represent a user
-    """
+    """Class to represent a user"""
 
-    def __init__(self, name: str):
+    def __init__(self, username: str, messages: list[str] = []):
 
-        self.name = name
-        self.messages = []  # list of all of the user's messages in the chat
-        for i, sender in enumerate(df["sender"]):
-            if sender != self.name: continue
-            self.messages.append(df["msg"][i])
+        self.username = username
+        self.messages = messages
 
-        self.num_messages, self.num_words, self.num_emojis = 0, 0, 0
-        self.word_freq, self.emoji_freq = {}, {}
         self.longest_msg = ""
-        max_len = 0
+        self.num_messages = self.num_words = self.num_emojis = 0
+        self.word_freq = self.emoji_freq = {}
 
         for msg in self.messages:
             self.num_messages += 1
-            if len(msg) > max_len:
-                max_len = len(msg)
-                self.longest_msg = msg
+            if not len(msg) > len(self.longest_msg): continue
+            self.longest_msg = msg
 
             for word in msg.split():
                 self.num_words += 1
-                if word not in self.word_freq: self.word_freq[word] = 1
-                else: self.word_freq[word] += 1
+                if word in self.word_freq: self.word_freq[word] += 1
+                else: self.word_freq[word] = 1
 
                 for char in word:
                     if not emoji.is_emoji(char): continue
                     self.num_emojis += 1
-                    if char not in self.emoji_freq: self.emoji_freq[char] = 1
-                    else: self.emoji_freq[char] += 1
+                    if char in self.emoji_freq: self.emoji_freq[char] += 1
+                    else: self.emoji_freq[char] = 1
 
-        self.word_emoji_ratio = self.num_emojis / self.num_words
-        self.avg_msg_len = self.num_words / self.num_messages  # words per message
+        if self.num_messages:
+            self.avg_msg_len = self.num_words / self.num_messages
         self.word_freq = dict(
             sorted(self.word_freq.items(), key=lambda x: x[1], reverse=True))
         self.emoji_freq = dict(
             sorted(self.emoji_freq.items(), key=lambda x: x[1], reverse=True))
 
-        hours = {hour:0 for hour in range(0, 24)}
-        for timestamp in df["timestamp"]:
-            print(timestamp)
 
-ansh = User("Ansh")
+def populate_user_data():
+    """Populates the user data"""
+
+    df = frame_data("testchat.txt")
+    users = [User(user) for user in df["user"].unique()]
+
+    for i, row in df.iterrows():
+        for user in users:
+            if user.username != row["user"]: continue
+            user.messages.append(row["message"])
+            break
+    print(users[0].messages)
+
+
+if __name__ == "__main__":
+    ...
