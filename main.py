4c4918ee10ef4d1018baa8a494668c40bee41238
Ansh <ansh.tiwatne@gmail.com>
2022-08-05 23:04:39 +0530

    added stacked graphs

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from colorama import Fore
 import emoji
 import pandas as pd
 
+DIVIDER = "="*48
 ENG_COMMON_WORDS = [
     #Articles
     'a', 'an', 'the',
@@ -74,7 +75,7 @@ class User:
         
         top_hour = self.hour_freq.most_common(1)[0][0]
         return f"""
-{self.username.upper()}{self.color}\n{"="*32}{Fore.RESET}
+{self.username.upper()}{self.color}\n{DIVIDER}{Fore.RESET}
 Messages sent: {self.color}{self.num_messages}{Fore.RESET}
 Avg msg length: {self.color}{self.avg_msg_len:.2f} {Fore.RESET}words
 Longest msg: {self.color}{len(self.longest_msg)}{Fore.RESET} chars
@@ -83,23 +84,47 @@ Emojis sent: {self.color}{self.num_emojis}{Fore.RESET}
 \nTOP WORDS:\n{self.graph_freq(self.word_freq, scale=150)}
 TOP EMOJIS:\n{self.graph_freq(self.emoji_freq, padding=1)}
 Most active at: {self.color}{dt.strptime(f"{top_hour}", "%H").strftime("%I:%M %p")}{Fore.RESET}
-Avg msg sentiment: {self.color}{"to do"}{Fore.RESET}
+Avg msg sentiment: {self.color}{NotImplemented}{Fore.RESET}
 """
+def graph_freq(self, freq: Counter, padding: int = 8, scale: int = 100):
+        """Returns a Unicode bar graph for a given frequency distribution"""
+
+        graph = ""
+        total = sum(freq.values())
+        for element, count in freq.most_common(5):
+            len_line = int(round(count / total * scale))
+            graph += f"{element:<{padding}} | {self.color}{'▇'*len_line}{Fore.RESET} {count}\n"
+        return graph
 
 
 def main(df: pd.DataFrame):
     """Main function"""
 
-    users = [user for user in df["user"].unique()]
+    users = []
+    usernames = [username for username in df["user"].unique()]
     colors = [
         Fore.LIGHTRED_EX, Fore.LIGHTGREEN_EX, Fore.LIGHTYELLOW_EX,
         Fore.LIGHTBLUE_EX, Fore.LIGHTMAGENTA_EX, Fore.LIGHTCYAN_EX
     ]
+    
     i = -1
-    for user in users:
+    for user in usernames:
         i += 1
         if i >= len(colors): i = 0
-        print(User(user, df, colors[i]))
+        user = User(user, df, colors[i])
+        users.append(user)
+        print(user)
+
+    total_msgs = sum(user.num_messages for user in users)
+    msg_bar = [f"{user.color}{'▇'*int(round(user.num_messages / total_msgs * 50))}{Fore.RESET}" for user in users]
+
+    print(f"""
+CHAT {" / ".join([f"{user.color}{user.username}{Fore.RESET}" for user in users])}\n{DIVIDER}\n
+Total words  | {''.join(msg_bar)} {':'.join([str(user.num_messages) for user in users])}
+Total emojis | {''.join(msg_bar)} {':'.join([str(user.num_emojis) for user in users])}
+\nTIMELINE:\n{NotImplemented}
+""")
+        
 
 
 if __name__ == "__main__":
