a977e0ccc0081eb69e0f524e2a1cf1e46742925e
Ansh <ansh.tiwatne@gmail.com>
2022-08-05 20:22:03 +0530

    switched to using Counter

--- main.py
+++ main.py
@@ -3,11 +3,10 @@
 from collections import Counter
 from datetime import datetime as dt
 import re
-from colorama import Fore
 import colorama
+from colorama import Fore
 import emoji
 import pandas as pd
-import termgraph.termgraph as tg
 
 
 def frame_data(path: str):
@@ -32,88 +31,61 @@ class User:
     """Class to represent a user"""
 
     def __init__(self, username: str, df: pd.DataFrame, color: str = Fore.RESET):
-
+        
         self.username = username
         self.color = color
-        self.longest_msg = ""
-        self.num_messages, self.num_words, self.num_emojis = 0, 0, 0
-        self.word_freq, self.emoji_freq = {}, {}
-        self.hour_freq = {h: 0 for h in range(24)}
+        self.longest_msg = max(df.loc[df["user"] == username]["message"], key=len)
+        self.word_freq = Counter(word.strip(r".,!\?;:()[]{}").lower() for msg in df.loc[df["user"] == username]["message"] for word in msg.split())
+        self.emoji_freq = Counter(char for msg in df.loc[df["user"] == username]["message"] for char in msg if emoji.is_emoji(char))
+        self.hour_freq = Counter(dt.strftime(timestamp, "%H") for timestamp in df.loc[df["user"] == username]["timestamp"])
+        self.num_messages = len(df.loc[df["user"] == username]["message"])
+        self.num_words = sum(self.word_freq.values())
+        self.num_emojis = sum(self.emoji_freq.values())
+        self.avg_msg_len = self.num_words/self.num_messages
 
-        for i, row in df.loc[df["user"] == username].iterrows():
-            timestamp, msg = row["timestamp"], row["message"]
-            self.num_messages += 1
-            if len(msg) > len(self.longest_msg): self.longest_msg = msg
+    def graph_freq(self, freq: Counter, padding: int = 8, scale: int = 100):
+        """Returns a Unicode bar graph for a given frequency distribution"""
 
-            for word in msg.split():
-                word = word.strip(r"\.\,\!\?\;\:\(\)\[\]\{\}").lower()
-                self.num_words += 1
-                if not emoji.is_emoji(word):
-                    if word in self.word_freq: self.word_freq[word] += 1
-                    else: self.word_freq[word] = 1
-
-                    for char in word:
-                        if not emoji.is_emoji(char): continue
-                        self.num_emojis += 1
-                        if char in self.emoji_freq: self.emoji_freq[char] += 1
-                        else: self.emoji_freq[char] = 1
-
-            hour = timestamp.hour
-            self.hour_freq[hour] += 1
-
-        if self.num_messages:
-            self.avg_msg_len = self.num_words / self.num_messages
-        self.hour_freq = dict(sorted(self.hour_freq.items(), key=lambda x: x))
-
-    def graph_freq(self, freq: dict, total: int, padding: int = 8, scale: int = 100):
-        """Draws a ascii graph for frequencies"""
-
-        freq = dict(sorted(freq.items(), key=lambda x: x[1], reverse=True))
         graph = ""
-        for key in list(freq)[:5]:
-            len_line = int(round(freq[key] / total * scale))
-            graph += f"{key:<{padding}} | {self.color}{'▇'*len_line}{Fore.RESET} {freq[key]}\n"
+        total = sum(freq.values())
+        for element, count in freq.most_common(5):
+            len_line = int(round(count / total * scale))
+            graph += f"{element:<{padding}} | {self.color}{'▇'*len_line}{Fore.RESET} {count}\n"
         return graph
-
+    
     def __repr__(self):
-        top_emojis = ""
-        for emoji in list(self.emoji_freq.keys())[:5]:
-            top_emojis += f"{emoji}:{self.color}{self.emoji_freq[emoji]}{Fore.RESET} "
-
-        top_hour = max(self.hour_freq.keys(), key=(lambda freq: self.hour_freq[freq]))
-        top_hour = dt.strptime(f"{top_hour}", "%H").strftime("%I:%M %p")
-
-        return (f"""{self.username.upper()}{self.color}\n{'='*32}{Fore.RESET}
+        
+        top_hour = self.hour_freq.most_common(1)[0][0]
+        return f"""
+{self.username.upper()}{self.color}\n{"="*32}{Fore.RESET}
 Messages sent: {self.color}{self.num_messages}{Fore.RESET}
-Avg msg length: {self.color}{self.avg_msg_len:.2f} words{Fore.RESET}
-Longest message: {self.color}{len(self.longest_msg)} chars{Fore.RESET}
+Avg msg length: {self.color}{self.avg_msg_len:.2f} {Fore.RESET}words
+Longest msg: {self.color}{len(self.longest_msg)}{Fore.RESET} chars
 Words sent: {self.color}{self.num_words}{Fore.RESET}
 Emojis sent: {self.color}{self.num_emojis}{Fore.RESET}
-\nTOP WORDS:\n{self.graph_freq(self.word_freq, self.num_words, scale=100)}
-\nTOP EMOJIS:\n{self.graph_freq(self.emoji_freq, self.num_emojis, padding=1)}
-Most active at: {self.color}{top_hour}{Fore.RESET}
-Avg msg sentiment: to do""")
+\nTOP WORDS:\n{self.graph_freq(self.word_freq)}
+TOP EMOJIS:\n{self.graph_freq(self.emoji_freq, padding=1)}
+Most active at: {self.color}{dt.strptime(f"{top_hour}", "%H").strftime("%I:%M %p")}{Fore.RESET}
+Avg msg sentiment: {self.color}{"to do"}{Fore.RESET}
+"""
 
 
-def draw(df: pd.DataFrame):
-    """Draws a graph of the data"""
+def main(df: pd.DataFrame):
+    """Main function"""
 
     users = [user for user in df["user"].unique()]
     colors = [
         Fore.LIGHTRED_EX, Fore.LIGHTGREEN_EX, Fore.LIGHTYELLOW_EX,
         Fore.LIGHTBLUE_EX, Fore.LIGHTMAGENTA_EX, Fore.LIGHTCYAN_EX
     ]
-
     i = -1
     for user in users:
         i += 1
-        if i > len(colors): i = 0
-        color = colors[i]
-        print(f"\n{User(user, df, color)}")
-    print(end=None)
+        if i >= len(colors): i = 0
+        print(User(user, df, colors[i]))
 
 
 if __name__ == "__main__":
     colorama.init(autoreset=True)
     df = frame_data("testchat.txt")
-    draw(df)
+    main(df)
